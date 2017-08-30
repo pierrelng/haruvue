c431869ff928bf92597f7b0d07bf2e3af6ca9bdb
pierrelng <langerreip@gmail.com>
2017-08-30 14:48:43 +0200

    Add infinite scroll to homepage and datetime filtering

--- src/components/Home/EventList/EventList.vue
+++ src/components/Home/EventList/EventList.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    <div v-for="event in events" class="list">
-      <span v-html="event.title.rendered"></span>
-      <a v-bind:href="event.acf.facebook_event_url">Facebookkkkkkkkkkk</a>
-      <img v-bind:src="event.details.cover_source">
-    </div>
+    <ul class="list"
+        v-infinite-scroll="loadMore"
+        infinite-scroll-disabled="loading"
+        infinite-scroll-distance="100"
+        infinite-scroll-immediate-check="false">
+      <li v-for="event in events">
+        <span v-html="event.title.rendered"></span>
+        <span>
+          {{ [event.details.start_time[0], 'YYYY-MM-DD HH:mm:ss'] | moment('dddd DD MMM [de] HH:mm') }}
+          -
+          {{ [event.details.end_time[0], 'YYYY-MM-DD HH:mm:ss'] | moment('HH:mm') }}
+        </span>
+        <a v-bind:href="event.acf.facebook_event_url">
+          <img v-bind:src="event.details.cover_source">
+        </a>
+      </li>
+    </ul>
+    <p v-show="loading" class="spinner">
+      <mt-spinner type="triple-bounce"></mt-spinner>
+    </p>
   </div>
 </template>
 
@@ -16,6 +31,8 @@ export default {
   data() {
     return {
       events: [],
+      offset: 0,
+      loading: false,
     };
   },
   created() {
@@ -23,10 +40,23 @@ export default {
   },
   methods: {
     getEvents() {
-      axios.get('https://pierrelange.com/wp-json/haru/v1/events')
+      axios.get(`https://pierrelange.com/wp-json/haru/v1/events?offset=${this.offset}`)
       .then((response) => {
         this.events = response.data;
       });
+    },
+    loadMore() {
+      this.loading = true;
+      setTimeout(() => {
+        this.offset = this.events.length;
+        axios.get(`https://pierrelange.com/wp-json/haru/v1/events?offset=${this.offset}`)
+        .then((response) => {
+          response.data.forEach((event) => {
+            this.events.push(event);
+          });
+          this.loading = false;
+        });
+      }, 5);
     },
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import { InfiniteScroll, Spinner } from 'mint-ui'; // http://mint-ui.github.io/docs/#/en2/infinite-scroll
+import 'mint-ui/lib/style.css'; // http://mint-ui.github.io/docs/#/en2/quickstart
 import App from './App';
 import router from './router';
+
+Vue.use(InfiniteScroll);
+Vue.component(Spinner.name, Spinner); // http://mint-ui.github.io/docs/#/en2/spinner
+
+const moment = require('moment'); // https://github.com/brockpetrie/vue-moment#configuration
+require('moment/locale/fr');
+Vue.use(require('vue-moment'), {
+  moment,
+});
 
 Vue.config.productionTip = false;
 
