6380cfb73c40d101c7098a3e45fe0e2574a108fe
pierrelng <langerreip@gmail.com>
2017-08-31 12:22:54 +0200

    Add dynamic date picker

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -7,17 +7,15 @@
     </form>
 
     <!-- Date picker -->
-    <div class="wrapper">
-      <ul class="datepicker">
-        <li class="datepicker__all">∞</li>
-        <li class="datepicker__today">Auj</li>
-        <li class="datepicker__other">Mercredi</li>
-        <li class="datepicker__other">Jeudi</li>
-        <li class="datepicker__other">Vendredi</li>
-        <li class="datepicker__other">Samedi</li>
-      </ul>
-      <!-- <span>{{ now }}</span> -->
-    </div>
+    <!-- <div class="wrapper"> -->
+    <ul class="datepicker scroll-x-wrapper">
+      <li class="datepicker__all scroll-x-item">∞</li>
+      <li class="scroll-x-item">Auj.</li>
+      <li class="scroll-x-item">Dem.</li>
+      <li class="scroll-x-item" v-for="day in days">{{ day }}</li>
+      <li class="scroll-x-item">Semaine suivante</li>
+    </ul>
+    <!-- </div> -->
 
     <event-list></event-list>
 
@@ -36,14 +34,32 @@ export default {
   data() {
     return {
       now: '',
+      days: [],
     };
   },
   created() {
-    setInterval(this.getNow, 1000);
+    // setInterval(this.getNow, 1000);
+    this.setDays();
   },
   methods: {
     getNow() {
-      this.now = moment().format('MMMM Do YYYY, h:mm:ss a');
+      this.now = moment().format('MMMM DD YYYY, h:mm:ss a');
+    },
+    setDays() {
+      const sunday = moment().endOf('week');
+      const daysUntilSunday = sunday.diff(moment(), 'days');
+      if (daysUntilSunday > 1) {
+        for (let i = 2; i <= daysUntilSunday; i += 1) {
+          this.days.push(moment().add(i, 'days').format('dddd'));
+        }
+      } else {
+        const endOfNextWeek = moment().endOf('week').add(7, 'days');
+        const daysUntilEndOfNextWeek = endOfNextWeek.diff(moment(), 'days');
+        for (let i = 2; i <= daysUntilEndOfNextWeek; i += 1) {
+          this.days.push(moment().add(i, 'days').format('dddd'));
+        }
+      }
+      // console.log(this.days);
     },
   },
 };
