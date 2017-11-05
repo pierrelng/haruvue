92f096f9a597d1336e10a1ae0f3c6e73d29e9775
pierrelng <pierrolng@gmail.com>
2017-11-05 15:35:08 +0100

    Add cover, title and calls to actions for single event page

--- src/components/Event/Event.vue
+++ src/components/Event/Event.vue
@@ -1,7 +1,60 @@
 <template>
   <div>
-    <h1>{{ $route.params.id }}</h1>
-    <p>{{ this.event }}</p>
+    <!-- Img with player -->
+    <img :src="event.details.cover_source">
+    <div class="play"></div>
+
+    <!-- Date + Title -->
+    <div class="col headline">
+      <div class="date-start">
+        {{ [event.details.start_time[0], 'YYYY-MM-DD HH:mm:ss'] | moment('dddd DD MMM') }}
+        {{ [event.details.start_time[0], 'YYYY-MM-DD HH:mm:ss'] | moment(' [de] HH:mm') }}
+        - {{ [event.details.end_time[0], 'YYYY-MM-DD HH:mm:ss'] | moment('HH:mm') }}
+      </div>
+      <div class="title" v-html="event.title.rendered"></div>
+    </div>
+
+    <!-- Calls to action -->
+    <div class="callsToAction">
+      <!-- Share -->
+      <div class="share disabled">
+        <i class="material-icons">share</i>
+        <span>Partager</span>
+      </div>
+      <!-- Star -->
+      <div class="star disabled">
+        <i class="material-icons">star_border</i>
+        <span>Interessé(e)</span>
+      </div>
+      <!-- Location -->
+      <div v-if="venue.acf.address" class="address">
+        <i class="material-icons">place</i>
+        <span>{{ venue.acf.address }}</span>
+        <a :href="'http://maps.google.com/?q=' + venue.acf.address">
+          <div class="map"></div>
+        </a>
+      </div>
+      <!-- Tickets -->
+      <a v-if="event.acf.billetterie_url" class="tickets" :href="event.acf.billetterie_url">
+        <i class="material-icons">credit_card</i>
+        <span>Billetterie</span>
+      </a>
+      <div v-else class="tickets disabled">
+        <i class="material-icons">credit_card</i>
+        <span>Billetterie</span>
+      </div>
+      <!-- Guestlist -->
+      <a v-if="event.acf.guestlist_url" class="guestlist" :href="event.acf.guestlist_url">
+        <i class="material-icons">assignment</i>
+        <span>Guestlist</span>
+      </a>
+      <div v-else class="guestlist disabled">
+        <i class="material-icons">assignment</i>
+        <span>Guestlist</span>
+      </div>
+    </div>
+
+    <!-- NAME -->
   </div>
 </template>
 
@@ -13,6 +66,7 @@ export default {
   data() {
     return {
       event: '',
+      venue: '',
     };
   },
   created() {
@@ -23,10 +77,18 @@ export default {
       axios.get(`https://pierrelange.com/wp-json/haru/v1/events/${this.$route.params.id}`)
       .then((response) => {
         this.event = response.data;
+        this.getVenue();
+      });
+    },
+    getVenue() {
+      axios.get(`https://pierrelange.com/wp-json/haru/v1/venues/${this.event.acf.venue[0].ID}`)
+      .then((response) => {
+        this.venue = response.data;
+        // console.log(this.venue);
       });
     },
   },
 };
 </script>
 
-<style scoped lang="stylus" src=""></style>
+<style scoped lang="stylus" src="./Event.styl"></style>
