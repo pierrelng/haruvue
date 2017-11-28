ce85c21a44f9fcc59070a6e5d1df6858afc398d0
pierrelng <pierrolng@gmail.com>
2017-11-28 13:47:15 +0100

    Create and display component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <bottom-nav></bottom-nav>
   </div>
 </template>
 
 <script>
+import BottomNav from '@/components/BottomNav/BottomNav';
+
 export default {
   name: 'app',
+  components: {
+    'bottom-nav': BottomNav,
+  },
 };
 </script>
 
---
+++ src/components/BottomNav/BottomNav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bottom-nav">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bottomNav',
+};
+</script>
+
+<style lang="stylus" src="./BottomNav.styl"></style>
