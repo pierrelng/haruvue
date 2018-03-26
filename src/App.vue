d6146a20fea9f5004a4db48160d90dc1bfecbf19
pierrelng <pierrolng@gmail.com>
2018-03-26 21:39:08 +0200

    Work in progress for bottom navigation

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <router-view></router-view>
     <bottom-player></bottom-player>
+    <!-- <bottom-nav></bottom-nav> -->
   </div>
 </template>
 
 <script>
 import BottomPlayer from '@/components/BottomPlayer/BottomPlayer';
+import BottomNav from '@/components/BottomNav/BottomNav';
 
 export default {
   name: 'app',
   components: {
     'bottom-player': BottomPlayer,
+    'bottom-nav': BottomNav,
   },
 };
 </script>
---
+++ src/components/BottomNav/BottomNav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bottom-nav">
+    mlqksdjf lkjqdsf lkj qmlsdfj
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bottomNav',
+};
+</script>
+
+<style scoped lang="stylus" src="./BottomNav.styl"></style>
